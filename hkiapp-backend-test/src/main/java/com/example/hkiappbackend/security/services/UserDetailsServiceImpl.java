7a55ea54a1b9c53d7e45ebd3357ce72c92ec3399
Josephine <josephine.closan@yahoo.com>
2022-06-16 14:03:38 +0300

    rename app test

---
+++ hkiapp-backend-test/src/main/java/com/example/hkiappbackend/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.hkiappbackend.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.hkiappbackend.models.User;
+import com.example.hkiappbackend.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+    return UserDetailsImpl.build(user);
+  }
+
+}
---
+++ hkiapp-backend-test/src/test/java/com/example/hkiappbackend/HkiappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hkiappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkiappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
